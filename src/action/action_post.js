64779d05a3601a723b66631f46d258c61c1fb47e
tails5555 <tails5555@naver.com>
2018-11-20 13:57:40 +0900

    Create Post Add Action, Reducer 게시글을 추가하는데 앞서 실제 django 서버와의 연동 작업이 완료 되었음. 그리고 action 과 reducer 설정도 작성함.

--- src/action/action_post.js
+++ src/action/action_post.js
@@ -17,6 +17,23 @@ function get_post_list_by_query_api(queryModel) {
     });
 }
 
+function create_post_api(postModel){
+    const requestBody = {
+        id : 0,
+        title : postModel && postModel.title,
+        writer : postModel && postModel.writer,
+        type : postModel && postModel.type,
+        context : postModel && postModel.context,
+        created_at : null,
+        updated_at : null
+    };
+    return axios({
+        url : ROOT_URL,
+        data : requestBody,
+        method : 'post'
+    });
+}
+
 function get_post_single_api(id){
     return axios({
         url : `${ROOT_URL}/${id}`,
@@ -52,4 +69,43 @@ const fetch_post_list_by_query_success = (response) => ({
 const fetch_post_list_by_query_failure = (error) => ({
     type : FETCH_POST_LIST_BY_QUERY_FAILURE,
     payload : error
+});
+
+export const CREATE_POST_CONTEXT = 'CREATE_POST_CONTEXT';
+export const CREATE_POST_CONTEXT_SUCCESS = 'CREATE_POST_CONTEXT_SUCCESS';
+export const CREATE_POST_CONTEXT_FAILURE = 'CREATE_POST_CONTEXT_FAILURE';
+export const RESET_CREATE_POST_CONTEXT = 'RESET_CREATE_POST_CONTEXT';
+
+export const create_post_context = (postModel) => (dispatch) => {
+    dispatch(create_post_context_trying());
+    
+    return create_post_api(postModel).then((response) => {
+        setTimeout(() => {
+            dispatch(create_post_context_success(response));
+        }, 2000);
+    }).catch(error => {
+        dispatch(create_post_context_failure(error.message));
+    });
+}
+
+export const reset_create_post_context = () => (dispatch) => {
+    dispatch(reset_create_post_context_trying());
+}
+
+const create_post_context_trying = () => ({
+    type : CREATE_POST_CONTEXT
+});
+
+const create_post_context_success = (response) => ({
+    type : CREATE_POST_CONTEXT_SUCCESS,
+    payload : response.data
+});
+
+const create_post_context_failure = (error) => ({
+    type : CREATE_POST_CONTEXT_FAILURE,
+    payload : error
+});
+
+const reset_create_post_context_trying = () => ({
+    type : RESET_CREATE_POST_CONTEXT
 })
--- src/reducer/reducer_post.js
+++ src/reducer/reducer_post.js
@@ -1,9 +1,11 @@
 import {
-    FETCH_POST_LIST_BY_QUERY, FETCH_POST_LIST_BY_QUERY_SUCCESS, FETCH_POST_LIST_BY_QUERY_FAILURE
+    FETCH_POST_LIST_BY_QUERY, FETCH_POST_LIST_BY_QUERY_SUCCESS, FETCH_POST_LIST_BY_QUERY_FAILURE,
+    CREATE_POST_CONTEXT, CREATE_POST_CONTEXT_SUCCESS, CREATE_POST_CONTEXT_FAILURE, RESET_CREATE_POST_CONTEXT
 } from '../action/action_post';
 
 const INITIAL_STATE = {
-    postList : { loading : false, posts : [], error : null, count : 0 }
+    postList : { loading : false, posts : [], error : null, count : 0 },
+    postSave : { loading : false, post : null, error : null }
 };
 
 export default function(state = INITIAL_STATE, action) {
@@ -15,6 +17,15 @@ export default function(state = INITIAL_STATE, action) {
             return { ...state, postList : { loading : false, error : null, posts : results, count }};
         case FETCH_POST_LIST_BY_QUERY_FAILURE :
             return { ...state, postList : { ...state.postList, loading : false, error : action.payload }};
+
+        case CREATE_POST_CONTEXT :
+            return { ...state, postSave : { loading : true, error : null, post : null }};
+        case CREATE_POST_CONTEXT_SUCCESS :
+            return { ...state, postSave : { loading : false, post : action.payload, error : null }};
+        case CREATE_POST_CONTEXT_FAILURE :
+            return { ...state, postSave : { ...state.postSave, loading : false, error : action.payload }};
+        case RESET_CREATE_POST_CONTEXT :
+            return { ...state, postSave : { loading : false, error : null, post : null }};
 
         default :
             return state;
